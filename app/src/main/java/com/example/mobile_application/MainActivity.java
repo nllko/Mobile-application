1c6117c36b8d25a30beb24918266dce11eb9972e
f0rsakenOne <nikolaj.aleksejenkov@gmail.com>
2022-10-07 16:14:25 +0300

    fixed services not displaying

--- app/src/main/java/com/example/mobile_application/MainActivity.java
+++ app/src/main/java/com/example/mobile_application/MainActivity.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 
 import com.example.mobile_application.model.Sport;
 
@@ -19,14 +22,16 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.atomic.AtomicReference;
+import java.util.concurrent.Future;
 
 public class MainActivity extends AppCompatActivity {
 
+    RecyclerView recyclerView;
     URL servicesUrl = new URL("https://engine.free.beeceptor.com/api/getServices");
-    ArrayList<Sport> sports = new ArrayList<>();
 
     public MainActivity() throws MalformedURLException {
     }
@@ -35,40 +40,53 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        recyclerView = findViewById(R.id.recyclerView);
         LinearLayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
         recyclerView.setLayoutManager(layoutManager);
 
-        getServicesJson();
-        CustomAdapter customAdapter = new CustomAdapter(MainActivity.this,sports);
-        recyclerView.setAdapter(customAdapter);
+        try {
+            ArrayList<Sport> sports = convertJsonToSportArray(getServicesJson());
+            CustomAdapter customAdapter = new CustomAdapter(getApplicationContext(),sports);
+            recyclerView.setAdapter(customAdapter);
+        } catch (ExecutionException | InterruptedException | JSONException e) {
+            e.printStackTrace();
+        }
 
     }
 
-    private void getServicesJson(){
+    private JSONArray getServicesJson() throws ExecutionException, InterruptedException {
         ExecutorService service = Executors.newSingleThreadExecutor();
-        service.execute(() -> {
+        Future<JSONArray> jsonArray = service.submit(() -> {
+            String json = null;
             try {
                 HttpURLConnection connection = (HttpURLConnection) servicesUrl.openConnection();
                 connection.setRequestMethod("GET");
 
                 BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
                 String inputLine;
-                StringBuffer response = new StringBuffer();
+                StringBuilder response = new StringBuilder();
 
                 while ((inputLine = in.readLine()) != null) {
                     response.append(inputLine);
                 }
                 in.close();
-                JSONArray jsonArray = new JSONArray(response.toString());
-                for (int i = 0; i < jsonArray.length(); i++) {
-                    JSONObject object = jsonArray.getJSONObject(i);
-                    Sport sport = new Sport(object.getString("name"),object.getInt("id"));
-                    sports.add(sport);
-                }
-            } catch (IOException | JSONException e) {
+                json = response.toString();
+
+            } catch (IOException e) {
                 e.printStackTrace();
             }
+            return new JSONArray(json);
         });
+        return jsonArray.get();
+    }
+
+    private ArrayList<Sport> convertJsonToSportArray(JSONArray jsonArray) throws JSONException {
+        ArrayList<Sport> sports = new ArrayList<>();
+        for (int i = 0; i < jsonArray.length(); i++) {
+            JSONObject object = jsonArray.getJSONObject(i);
+            Sport sport = new Sport(object.getString("name"), object.getInt("id"));
+            sports.add(sport);
+        }
+        return sports;
     }
 }
