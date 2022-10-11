45bd29f7bd713fb2107a233a16b15864f9b4b318
f0rsakenOne <nikolaj.aleksejenkov@gmail.com>
2022-10-11 16:12:10 +0300

    changed ui and added sport images

--- app/src/main/java/com/example/mobile_application/CustomAdapter.java
+++ app/src/main/java/com/example/mobile_application/CustomAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,10 +13,15 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.mobile_application.model.Sport;
 
 import java.util.ArrayList;
-import java.util.concurrent.CopyOnWriteArrayList;
 
 public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.MyViewHolder> {
     ArrayList<Sport> sports;
+    int[] sportsImages = {R.drawable.ic_baseline_sports_football_24,
+            R.drawable.ic_baseline_sports_basketball_24,
+            R.drawable.ic_baseline_sports_cricket_24,
+            R.drawable.ic_baseline_sports_mma_24,
+            R.drawable.ic_baseline_sports_rugby_24,
+            R.drawable.ic_baseline_sports_soccer_24};
     Context context;
 
     public CustomAdapter(Context context, ArrayList<Sport> sports) {
@@ -26,7 +32,7 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.MyViewHold
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_layout,parent,false);
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_layout, parent, false);
         MyViewHolder viewHolder = new MyViewHolder(v);
         return viewHolder;
     }
@@ -34,6 +40,7 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.MyViewHold
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
         holder.name.setText(sports.get(position).getName());
+        holder.image.setImageResource(sportsImages[position]);
     }
 
     @Override
@@ -43,9 +50,12 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.MyViewHold
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
         TextView name;
+        ImageView image;
+
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
             name = itemView.findViewById(R.id.name);
+            image = itemView.findViewById(R.id.image);
         }
     }
 }
